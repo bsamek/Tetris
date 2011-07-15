ba41a78a63ca9c8451f22eb6900629b2c691ddc0
Brian Samek <brian.samek@gmail.com>
2011-07-15 17:41:17 -0400

    Edited comments for all classes and methods.

--- tetris.py
+++ tetris.py
@@ -7,7 +7,16 @@ class Game():
     SPEED = 100
 
     def start(self):
-        # Create the main window and the canvas
+        '''Starts the game.
+
+        1. Creates game window of WIDTH and HEIGHT
+        2. Creates canvas
+        3. Creates first shape on canvas
+        4. Starts GUI timer of ms interval SPEED
+        5. Starts the GUI main loop
+        
+        '''
+        # Creates the main window and the canvas
         self.root = tk.Tk()
         self.root.title("Tetris")
         self.canvas = tk.Canvas(
@@ -16,22 +25,29 @@ class Game():
                 height=Game.HEIGHT)
         self.canvas.pack()
 
-        # Create a first shape
+        # Creates a first shape
         self.current_shape = self.create_shape()
 
-        # Start the timer and the GUI
+        # Starts the timer and the GUI
         self.timer()
         self.root.mainloop()
     
     def timer(self):
+        '''Every Game.SPEED ms, attempt to cause the current_shape to fall().
+
+        If fall() returns False, create a new shape.
+        
+        '''
         self.root.after(Game.SPEED, self.timer)
         if not self.current_shape.fall(self.canvas):
             self.current_shape = self.create_shape()
         
     def create_shape(self):
+        '''Create a new shape.'''
         return Shape(self.canvas)
 
 class Shape:
+    '''Defines a tetris shape.'''
     BOX_SIZE = 20
     START_POINT = Game.WIDTH / 2 / BOX_SIZE * BOX_SIZE - BOX_SIZE
     SHAPES = (
@@ -45,8 +61,18 @@ class Shape:
             )
 
     def __init__(self, canvas):
-        self.boxes = []
-        self.shape = choice(Shape.SHAPES)
+        '''Create a shape.
+
+        For each point in the shape definition given in the SHAPES dictionary,
+        create a rectangle of size BOX_SIZE. Save the integer references to
+        these rectangles in the self.boxes list.
+
+        Args:
+        canvas - the parent canvas on which the shape appears
+
+        '''
+        self.boxes = [] # the squares drawn by canvas.create_rectangle()
+        self.shape = choice(Shape.SHAPES) # a random shape
         self.color = self.shape[0]
 
         for point in self.shape[1:]:
@@ -59,15 +85,22 @@ class Shape:
             self.boxes.append(box)
 
     def move(self, canvas, x, y):
-        '''Move this shape (x, y) boxes.'''
+        '''Move this shape (x, y) boxes.
+
+        For each box in the list self.boxes, get its coordinates. If the box is
+        at the bottom of the screen, return False. If moving the box would
+        cause it to overlap an existing box, return False. Otherwise, move the
+        box (x, y) * BOX_SIZE and return True.
+
+        '''
         for box in self.boxes:
             coords = canvas.coords(box)
             
-            # Return False if shape is at bottom of screen
+            # Returns False if shape is at bottom of screen
             if coords[3] == Game.HEIGHT:
                 return False
 
-            # Return False if moving box (x, y) would overlap another box
+            # Returns False if moving box (x, y) would overlap another box
             overlap = set(canvas.find_overlapping(
                     (coords[0] + coords[2]) / 2 + x * Shape.BOX_SIZE, 
                     (coords[1] + coords[3]) / 2 + y * Shape.BOX_SIZE, 
@@ -77,7 +110,7 @@ class Shape:
             other_items = set(canvas.find_all()) - set(self.boxes)
             if overlap & other_items: return False
         
-        # Move the boxes
+        # Moves the boxes
         for box in self.boxes:
             canvas.move(box, x * Shape.BOX_SIZE, y * Shape.BOX_SIZE)
         return True
